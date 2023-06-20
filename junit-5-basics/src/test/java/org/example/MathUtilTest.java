4b862672374059dbcb209256ae167fc49c0d2755
Tushar7pute <tushsat.007ts@gmail.com>
2023-06-20 16:38:38 +0530

    Added first Junit test with assertEquals() method

---
+++ junit-5-basics/src/test/java/org/example/MathUtilTest.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilTest {
+
+    /*
+    Testing is basically checking whether the achieved output is equal to the expected one.
+    Step 1: Create the instance of the class under test
+    Step 2: set up inputs
+    Step 3: Execute the code you want
+    Step 4: Verify the achieved results with expected results
+     */
+
+    @Test
+    public void additionTest() {
+
+        //* Step 1: create the instance of the class under test
+        MathUtil mathUtil = new MathUtil();
+
+        //* Step 2: set up inputs
+        int firstNumber = 1, secondNumber = 1;
+        int expectedOutput = 2;
+
+        //* Step 3: Execute the code you want
+        int achievedOutput = mathUtil.add(firstNumber, secondNumber);
+
+        //* Step 4: Verify the achieved results with expected results
+        assertEquals(expectedOutput, achievedOutput);
+
+
+        /*
+            The method also have an addition string parameter which is used to display message
+            in case of test failures.
+
+            e.g.
+            assertEquals(expectedOutput, achievedOutput,"The method should add given input." );
+        */
+    }
+}
